76d7446f4cfda3cbeed95aee7f8e2328ea2f2a68
Shraddha2702 <shraddhak102@gmail.com>
2017-05-11 23:38:33 +0530

    commit

--- Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/ann_template.py
+++ Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/ann_template.py
@@ -12,11 +12,29 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('Churn_Modelling.csv')
 
-X = dataset.iloc[:,[2,3]].values
-y = dataset.iloc[:,-1].values
+X = dataset.iloc[:,3:13].values
+y = dataset.iloc[:,13].values
 
 
-from sklearn.cross_validation import train_test_split
+
+#Encoding the Categorical data
+#Using the sklearn library, it automatically gives it indexes just by fitting
+#OneHotEncoder makes different no of columns for differentiating / dumpy variables
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:,1] = labelencoder_X_1.fit_transform(X[:, 1])
+
+labelencoder_X_2 = LabelEncoder()
+X[:,2] = labelencoder_X_2.fit_transform(X[:, 2])
+
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+
+X = X[:,1:]
+
+
+
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, 
                                                     random_state = 0)
 
@@ -38,4 +56,3 @@ y_pred = classifier.predict(X_test)
 #Make Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
-
